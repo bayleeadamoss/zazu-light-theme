fff6c3caec31523eeaa9dc11620e3873894fd5cd
Blaine Schmeisser <blainesch@gmail.com>
2016-05-13 01:01:26 -0700

    Update to have feater icon.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var path = require('path');
+var inlineimage = require('gulp-inline-image');
 
 gulp.task('build', function () {
   return gulp.src('./sass/**/*.scss')
+    .pipe(inlineimage())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
 });
