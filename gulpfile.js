8845278a2f81934f52160f5e384870a5bc162517
Blaine Schmeisser <blainesch@gmail.com>
2016-05-12 23:51:26 -0700

    Convert to sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
 var path = require('path');
 
 gulp.task('build', function () {
-  return gulp.src('./less/**/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
 });
